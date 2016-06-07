58e43360e60d6f1a6adfbd17f6e788c80f09fd06
Mohamed Shanan <mohshanan87@gmail.com>
2016-06-07 16:55:45 +0200

    commit

--- app/src/main/java/com/renasoft/sociallogin/MainActivity.java
+++ app/src/main/java/com/renasoft/sociallogin/MainActivity.java
@@ -1,13 +1,13 @@
 package com.renasoft.sociallogin;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
+        import android.content.Intent;
+        import android.os.Bundle;
+        import android.support.v7.app.AppCompatActivity;
+        import android.view.View;
+        import android.widget.Button;
 
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.twitter.sdk.android.core.models.User;
+        import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+        import com.twitter.sdk.android.core.models.User;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
